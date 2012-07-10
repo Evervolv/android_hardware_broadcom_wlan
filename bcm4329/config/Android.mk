e085834a4a381411485c2327b60a1b59f6ff5d5e
andrew.boren <andrew.boren@gmail.com>
2012-07-09 21:54:00 -0700

    bcm4329: wpa_supplicant does not exist anymore, use wpa_supplicant_8 instead
    Change-Id: I2267d04047ca3e0f9e4e9b8d5337afc56d30d3bb

--- bcm4329/config/Android.mk
+++ bcm4329/config/Android.mk
@@ -31,7 +31,7 @@ WIFI_DRIVER_SOCKET_IFACE := wlan0
 ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_6_X)
   include external/wpa_supplicant_6/wpa_supplicant/wpa_supplicant_conf.mk
 else
-  include external/wpa_supplicant/wpa_supplicant_conf.mk
+  include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
 endif
 
 #######################
