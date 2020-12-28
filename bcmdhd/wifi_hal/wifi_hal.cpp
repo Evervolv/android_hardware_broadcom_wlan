ab18b02495a56f0cd4d7ab5dd10ea57d29816651
Mir Ali <mir-khizer.ali@broadcom.com>
2020-12-28 18:26:55 +0530

    Adding support for dedicated NMI interface
    A static network interface is registered for NMI operation.
    This change allows the HAL to recognize wlan specific interface
    during init.
    
    Bug: 163713613
    Test: Validated on Android S.
    
    Signed-off-by: Mir Ali <mir-khizer.ali@broadcom.com>
    Change-Id: I735c4a5af5cd2afe655fd3237637b47a2087cab8

--- bcmdhd/wifi_hal/wifi_hal.cpp
+++ bcmdhd/wifi_hal/wifi_hal.cpp
@@ -1467,7 +1467,8 @@ static int wifi_get_multicast_id(wifi_handle handle, const char *name, const cha
 
 static bool is_wifi_interface(const char *name)
 {
-    if (strncmp(name, "wlan", 4) != 0 && strncmp(name, "swlan", 5) != 0 && strncmp(name, "p2p", 3) != 0) {
+    if (strncmp(name, "wlan", 4) != 0 && strncmp(name, "swlan", 5) != 0 &&
+        strncmp(name, "p2p", 3) != 0 && strncmp(name, "aware", 5) != 0) {
         /* not a wifi interface; ignore it */
         return false;
     } else {
