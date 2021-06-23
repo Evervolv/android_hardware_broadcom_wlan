da66831e3ef87d5fabfc824b296a1087e1d170b2
Dennis Jeon <dennis.jeon@broadcom.com>
2021-06-23 12:05:17 +0900

    HAL: HAL: Increase netlink event socket buffer to avoid dropping packet
    Intermittenly, we can see "No buffer space available" in log.
    06-15 13:34:00.495 E WifiHAL : POLL Error; error no = 0 (Success)
    06-15 13:34:00.495 E WifiHAL : Read after POLL returned -1, error no = 105 (No buffer space available)
    
    We increased the socket buffer in vendor HAL before. pa/1835219
    
    At that time, 2MB socket buffer is enough to handle debug_dump (2M~4M) and other events.
    but, sometimes HAL daemon didn't get a chance to handle data due to some processes take control of resources.
    
    Increase event socket buffer from 2MB to 4MB
    
    BUG: 191137443
    Test: Device can connect to AP.
    Change-Id: Ib92192b863a9c585d9fdd53f42de6e5f6c54cce3

--- bcmdhd/wifi_hal/wifi_hal.cpp
+++ bcmdhd/wifi_hal/wifi_hal.cpp
@@ -409,7 +409,7 @@ wifi_error wifi_pre_initialize(void)
     }
 
     /* Set the socket buffer size */
-    if (nl_socket_set_buffer_size(event_sock, (2*1024*1024), 0) < 0) {
+    if (nl_socket_set_buffer_size(event_sock, (4*1024*1024), 0) < 0) {
         ALOGE("Could not set size for event_sock: %s",
                strerror(errno));
     } else {
